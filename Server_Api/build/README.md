a6659d69d6c45e01f77bd72477e3f21ad4334d99
RecRoger <recroger@gmail.com>
2020-08-03 19:07:12 -0300

    todo configurado para el deploy

---
+++ Server_Api/build/README.md
@@ -0,0 +1,5 @@
+# wos_build
+
+Este proyecto es el compilado buildeado de Wolrd of Stories
+Cuenta con el servidor, traducido a js del proyecto en ts
+Cuenta con el build del proyecto Angular
--- Server_Api/build/src/keys.js
+++ Server_Api/build/src/keys.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.mongoKeys = {
-    "URI": "mongodb://localhost/world_of_stories"
+    "URI": "mongodb://localhost/world_of_stories",
+    "URIprod": "mongodb+srv://WoS-MongoDB:nBVtVBB814lLkxzO@woscluster.tcjym.gcp.mongodb.net/wos-db?retryWrites=true&w=majority"
 };
