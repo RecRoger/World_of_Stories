c74d6bd4d0e83448a956d3ba1c8da8eefe08de3d
RecRoger <recroger@gmail.com>
2026-06-08 14:52:49 -0300

    chore: limpieza de archivos obsoletos y optimización de gitignore

--- Server_Api/src/keys.ts
+++
@@ -1,4 +0,0 @@
-export const mongoKeys = {
-    "URI": "mongodb://localhost/world_of_stories",
-    // "URIprod": "mongodb+srv://WoS-MongoDB:nBVtVBB814lLkxzO@woscluster.tcjym.gcp.mongodb.net/wos-db?retryWrites=true&w=majority"
-}
